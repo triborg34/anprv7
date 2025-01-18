234235a600e3bcde2ccb5aa55113c935a5bd6b39
triborg34 <you@example.com>
2025-01-18 10:52:40 +0330

    final v4

--- engine/setup.py
+++
@@ -1,8 +0,0 @@
-from cx_Freeze import setup, Executable
-
-setup(
-    name="MyProgram",
-    version="1.0",
-    description="My Python Program",
-    executables=[Executable("rtsp_streaming.py")],
-)
